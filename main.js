e56c2f9622397081a4a27e18f6f74bc8fdaac82a
EfrainLM <52606726+EfrainLM@users.noreply.github.com>
2026-03-29 23:16:08 -0600

    evento abrir invitación main.js

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 // Inicializar animaciones al hacer scroll
 AOS.init({ duration: 1000, once: true });
 
+const overlay = document.getElementById('overlay');
+const btnEntrar = document.getElementById('btn-entrar');
 const audio = document.getElementById('weddingSong');
-const btn = document.getElementById('playPauseBtn');
 
-// Manejar reproducción (los navegadores bloquean el autoplay)
-btn.addEventListener('click', () => {
-    if (audio.paused) {
-        audio.play();
-        btn.innerHTML = "⏸ Pausar Música";
-    } else {
-        audio.pause();
-        btn.innerHTML = "🎵 Reproducir Canción";
-    }
+btnEntrar.addEventListener('click', () => {
+    // 1. Reproducir audio (Ahora sí el navegador lo permite)
+    audio.play();
+    
+    // 2. Desvanecer la pantalla de bienvenida
+    overlay.classList.add('fade-out');
+    
+    // Opcional: Iniciar las animaciones de AOS después del clic
+    AOS.refresh();
 });
